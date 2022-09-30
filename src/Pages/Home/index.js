2b2a86a1095c4df746be2d6f5ae736c5f02c1677
Noob-Bot-1 <devbansal1512@gmail.com>
2022-09-30 18:41:37 +0530

    Added link to create event button

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,6 +1,7 @@
 import { Grid } from '@mui/material';
 import React from 'react'
 import '../../Styles/Home/index.css'
+import {Link} from 'react-router-dom';
 const url_src = 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/ea95af2d-7f06-4f25-859c-9069519053a7/Landing_page_image.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220930%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220930T090052Z&X-Amz-Expires=86400&X-Amz-Signature=dc06c2d0333e9c887aaa4fc2f3164eb09b416c665acf4d38003d025f4ddc0223&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Landing%2520page%2520image.svg%22&x-id=GetObject';
 
 function Home() {
@@ -22,7 +23,9 @@ function Home() {
           <img className = "img1" src = {url_src} />
         </div>
         <div className='button-div'>
-          <button className='create-event-button'>🎉 Create my event</button>
+          <Link to='/create'>
+            <button className='create-event-button'>🎉 Create my event</button>
+          </Link>
         </div>
       
       </Grid>
